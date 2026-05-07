dcf853563ef6819cfbce43f3a752e68877447394
Yossef <ym97989390@gmail.com>
2026-05-07 21:44:37 +0300

    asignment 15

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ class Admin extends User {
         super(id, name, email, password, phone, age);
     }
 
-    public manageNotes(){
+    public manageNotes() {
         console.log("Admin can manage notes");
     }
 }
@@ -82,7 +82,7 @@ admin1.manageNotes()
 create a method called preview() that retuens a short review of content
  */
 
-class Note{
+class Note {
     public id: number;
     public title: string;
     public content: string;
@@ -103,4 +103,75 @@ class Note{
 
 const note1 = new Note(1, "Note 1", "This is the content of note 1. It is a long content that should be previewed.", user1);
 note1.author.displayInfo();
-console.log(note1.preview())
+console.log(note1.preview());
+
+/**Create a class called NoteBook
+Requirements:  The NoteBook class should contain a collection of Notes objects .
+ Implement methods such as : addNote() , removeNote()
+ The relationship between Notebook and Note must represent Composition */
+
+class NoteBook {
+    public name: string;
+    public notes: Note[];
+    constructor(name: string,notes: Note[]) {
+        this.name = name;
+        this.notes = notes;
+    }
+    public addNote(note: Note) {
+        this.notes.push(note);
+    }
+    public removeNote(noteName: string) {
+        for (const note in this.notes) {
+            if (this.notes[note]?.title === noteName) {
+                this.notes.splice(Number(note), 1);
+                console.log(`Note ${noteName} removed successfully`);
+                return;
+            }
+        }
+    }
+}
+
+const notebook1 = new NoteBook("My Notebook", [note1]);
+notebook1.addNote(new Note(12, "Note 2", "This is the content of note 2. It is a long content that should be previewed.", user1));
+console.log(notebook1.notes);
+
+/** 5. Aggregation – User and Notebook (1 Mark)
+Create a relationship between:
+ User
+ NoteBook
+
+Requirements:
+ A user can own multiple notebooks.
+ This relationship should represent Aggregation.*/
+
+class UserWithNotebooks{
+    public user: User;
+    public notebooks: NoteBook[];
+
+    constructor(user: User, notebooks: NoteBook[]) {
+        this.user = user;
+        this.notebooks = notebooks;
+    }
+}
+
+/** Create a Generic Class called :
+ Storage
+Requirements:  The class should be able to store any type of data . Example operations may include :
+1. addItem ()
+2. removeItem() */
+
+class Storage<T>{
+    public items:T[];
+    constructor(items:T[]) {
+        this.items = items;
+    }
+    public addItem(item:T) {
+        this.items.push(item);
+    }
+    public removeItem(item:T) {
+        const index = this.items.indexOf(item);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+    }
+}
